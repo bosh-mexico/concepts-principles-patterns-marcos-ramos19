d1ac38468839d4830843c6719e21b6d4878a73c0
marcos-ramos19 <yahelrl19@icloud.com>
2025-09-24 10:46:48 -0600

    Delete select_m_names.py

--- select_m_names.py
+++
@@ -1,5 +0,0 @@
-def select_names_starting_with_M(names: list[str], case_sensitive: bool = False) -> list[str]:
-    if case_sensitive:
-        return [name for name in names if name and name.startswith('M')]
-    else:
-        return [name for name in names if name and name.lower().startswith('m')]
